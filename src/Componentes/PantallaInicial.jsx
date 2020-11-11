10eb25e19e63065f14545a18453a74953d4806e4
solojoh151296 <54746679+solojoh151296@users.noreply.github.com>
2020-11-10 23:03:56 -0500

    Cambios en la zona de preguntas

--- src/Componentes/PantallaInicial.jsx
+++ src/Componentes/PantallaInicial.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { Grid, Button, AppBar, Toolbar, Typography, Container } from "@material-ui/core"
 import { Link } from "react-router-dom"
 import '../assets/css/FondoBeneficiario.css'
+
 function PantallaInicial () {
-    
     return (
         <div>
             <div className='Fondo'>
@@ -16,16 +16,16 @@ function PantallaInicial () {
                     </Toolbar>
                 </AppBar>
                 {/* <h1 style={{color: 'black', margin: 0,padding: 50}}>Consulte su Bono</h1> */}
-                             
-                    
+
+
                 {/* Título */}
                  <Grid container direction="row" justify="center">
                     <Grid container item xs={12} justify="center">
                         <Typography variant="h2"  gutterBottom justify="center" >
                             <h3 style={{color: 'black', margin: 0,justify:"center",padding: 50 }}>Consulte su Bono</h3>
-                        </Typography> 
-                    </Grid>                                                  
-                </Grid>             
+                        </Typography>
+                    </Grid>
+                </Grid>
                    {/* botones */}
                 <Grid container direction="row" justify="center">
                     <Grid container item md={2} justify="center">
@@ -60,18 +60,54 @@ function PantallaInicial () {
                     </Grid>
                 </Grid>
             </div>
+            <Grid container direction="row" justify="center">
+                <Grid container item xs={12} justify="center">
+                        <Typography variant="h2"  gutterBottom justify="center" >
+                            <h3 style={{color: 'black', margin: 0,justify:"center",padding: 20 }}>Preguntas</h3>
+                        </Typography>
+                </Grid>
+            </Grid>
             {/* contenedor clásico, fondo blanco redondeado */}
             <div className='Contenedor'>
-                <Container style={{margin: 100}}>
-                    <h1 color="inherit">Preguntas</h1>
-                    <h4 color="inherit">Pregunta 1</h4>
-                    <h4 color="inherit">Pregunta 2</h4>
-                    <h4 color="inherit">Pregunta 3</h4>
-                    <h4 color="inherit">Pregunta 4</h4>
+                 {/* Título */}
+
+                <Container style={{margin: 10}}>
+                    <Grid container direction="row" justify="flex-start">
+                        <Grid container item xs={12} style={{marginTop: 40}} justify="flex-start">
+                            <h4 color="inherit">Pregunta 1</h4>
+                        
+                        </Grid>
+                        <Grid container item xs={1} justify="flex-start"/>    
+                        <Grid container item xs={11} justify="flex-start">
+                             <h4 color="inherit">Respuesta 1</h4>                        
+                        </Grid>
+                      
+                    </Grid>
+                    <Grid container direction="row" justify="flex-start">
+                        <Grid container item xs={12} justify="flex-start">
+                            <h4 color="inherit">Pregunta 2</h4>
+                        </Grid>
+                        <Grid container item xs={1} justify="flex-start"/>    
+                        <Grid container item xs={11} justify="flex-start">
+                             <h4 color="inherit">Respuesta 2</h4>                        
+                        </Grid>
+                    </Grid>
+                    <Grid container direction="row" justify="flex-start">
+                        <Grid container item xs={12} justify="flex-start">
+                            <h4 color="inherit">Pregunta 3</h4>
+                        </Grid>
+                        <Grid container item xs={1} justify="flex-start"/>    
+                        <Grid container item xs={11} justify="flex-start">
+                             <h4 color="inherit">Respuesta 3</h4>                        
+                        </Grid>
+                    </Grid>
+                    
                 </Container>
             </div>
         </div>
     );
-    
+
+
+
 }
 export default PantallaInicial;
