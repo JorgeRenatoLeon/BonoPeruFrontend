3ac3acecf783d069a0c6bc9e655e593d89f14fd0
solojoh151296 <54746679+solojoh151296@users.noreply.github.com>
2020-12-02 02:14:43 -0500

    generacion del cronograma  v2+nombre y fecha input

--- src/Componentes/Representante/GestionBonos.jsx
+++ src/Componentes/Representante/GestionBonos.jsx
@@ -1,5 +1,5 @@
 import React, {setState} from 'react'
-import {  AppBar, Toolbar,Typography,  Container,TextField } from "@material-ui/core"
+import {  AppBar, Toolbar,Typography,  Container,TextField , InputBase, Paper} from "@material-ui/core"
  import { Grid, Button } from "@material-ui/core"
  import { Link } from "react-router-dom"
  import axios from "axios";
@@ -9,29 +9,49 @@ import {  AppBar, Toolbar,Typography,  Container,TextField } from "@material-ui/
 import Cronograma from './Cronograma';
 import { history } from "../../helpers/history";
 
+import { makeStyles } from "@material-ui/core/styles";
+import { textSpanContainsPosition } from 'typescript';
+
 function formato(texto){
     return texto.replace(/^(\d{4})-(\d{2})-(\d{2})$/g,'$3/$2/$1');
+  }
+  function formatoInverso(texto){ //Transformo desde DD/MM/AAAA a AAAA-MM-DD
+    console.log(texto);
+    var i=texto.indexOf('/',1); //Comienza desde 0
+    console.log(i);
+    var dia=texto.substring(0,i);
+    var j=(texto.substring(i+1,texto.length)).indexOf('/',1); //Comienza desde 0
+    var mes=(texto.substring(i+1,texto.length)).substring(0,j);
+    var anio=(texto.substring(i+1,texto.length)).substring(j+1,texto.length);
+
+    return anio +"-"+mes+"-"+dia;
   }
   const rpta = [
     { id: 'Respuesta',  label: 'Respuesta' },
    
   ];
-  const ARI_URL = "http://localhost:8084/api/cronograma/generarcronograma";
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      padding: "0px 0px",
+      display: "flex",
+      alignItems: "center",
+      width: 150,
+      borderRadius:100,
+      margin:0
+    },
+    input: {
+      marginLeft: theme.spacing(1),
+      flex: 1
+    },
+ 
+    
+  }));
+  const ARI_URL = "http://localhost:8084/api/cronograma/generarcronograma";//Ari
+  const API_URL = "http://localhost:8084/api/cronograma/resumencronograma";//Caro  
+  const PUB_URL = "http://localhost:8084/api/cronograma/publicar";//Ari
 
-//   cronograma ya generado
-  /*
-  var Arrcronograma = [{ fechaini:"2020-11-28", 
-                         fechafin:"2020-12-15",
-                         beneficiarios:1024,
-                         lugares:32,                       
-                        
-                        }]
-                        
-*/
-    // cronograma no iniciado
- // /*    
-  
- //*/   
+
+
   var updateCronograma=false;
  function GenerarCronograma(){
         updateCronograma=true; 
@@ -40,9 +60,16 @@ function formato(texto){
          //   if(cronograma===undefined || cronograma.length===0){
 
                  //   setCronograma(Arrcronograma);
+            const escribePantalla = JSON.parse(localStorage.getItem("escribePantalla")) ;    //La hemos obtenido 
+            const soloFecha = JSON.parse(localStorage.getItem("soloFecha")) ;    //La hemos obtenido 
+            const soloNombre = JSON.parse(localStorage.getItem("soloNombre")) ;    //La hemos obtenido 
+
+            console.log('pantalla',soloFecha);
             const params=     {
-                    nombre:"BonoSoloJoh", //<3
-                    fechaini:"2020-12-3", //AAAA-MM-DD
+                    // nombre:"BonoSoloJoh", //<3
+                    nombre:soloNombre, 
+                    // fechaini:"2020-12-3", //AAAA-MM-DD
+                     fechaini:formatoInverso(escribePantalla.FechaInicio), //AAAA-MM-DD
                     fechafin:"",
                     usuariocreacion:1
             }
@@ -63,41 +90,62 @@ function formato(texto){
             .catch(() => {
                 console.log('Error al obtener Cronograma generado')
             });
-        
-
-
-          //  }
-        //},  [])
-
-
  }    
-  
- const API_URL = "http://localhost:8084/api/cronograma/resumencronograma";
+ function PublicarCronograma(){
+        const cronGuardado = JSON.parse(localStorage.getItem("cronogramaKaytlin")) ;    //La hemos obtenido 
 
- var rptaAPI;
+            //Sorry 
+        const params=     {
+                idcronograma:cronGuardado.idcronograma
+        }
+          //   API de Ari
+        axios.post(PUB_URL+"/"+cronGuardado.idcronograma)
+        .then(response =>{
+            console.log("ARI Pub url ",response);
+          
+        })
+        .catch(() => {
+            console.log('Error al obtener Publicar cronograma')
+        });
+}  
  
+
+
+function guardarFecha(event){
+    
+    localStorage.setItem("soloFecha",event); 
+    console.log('e: ',event);
+}
+function guardarNombre(event){
+    
+    localStorage.setItem("soloNombre",event); 
+    console.log('e: ',event);
+}
+
 //  path: /bonos
 function GestionBonos (props) {
   //useState devuelve 2 valores, en la pos 0, devuelve  el valor, y el la pos 1, devuelve una función
-        //  const classes = useStyles();
+          const classes = useStyles();
         var respuesta;
         const [fechaInicioAux,setFechaIni] = useState("2020-11-20");
         const [fechaFinAux, setFechaFin] = useState("2020-12-10");
         const [idcronograma,setIdCronograma] =useState(2);
         const [cronograma,setCronograma]=useState([{    
-                                "beneficiarios": 0,
+                                "beneficiarios": "Cargando...",
                                 "fechaini": "",
-                                "idcronograma": "",
-                                "lugares": 0,
+                                "idcronograma": "inicial",
+                                "lugares": "Cargando...",
                                 "fechafin": ""
 
         }]); //Set cronograma, creando y un estado de toda la función
         updateCronograma=false;
+    
+       
             useEffect((cronograma) => {
 
                 //Para que se actualice y mande a la pantalla principal
                 if(cronograma===undefined || cronograma.length===0){
-
+                 
                 // /*     API API API API API
                  axios.post(API_URL)
                  .then(response =>{
@@ -111,8 +159,10 @@ function GestionBonos (props) {
                         if(apiCronograma){
                             setCronograma(apiCronograma);
                             console.log("API OBT cro: ",apiCronograma);
+                            
                             if(apiCronograma[0].idcronograma===""){
                                 updateCronograma=true;
+                               
                             }
                             
                         }
@@ -133,39 +183,27 @@ function GestionBonos (props) {
                             lugares:"",      //Vendrán null              
                 
                         }];
-                        /*
-                        {
-                                "beneficiarios": 3,
-                                "fechaini": "",
-                                "idcronograma": "",
-                                "lugares": 0,
-                                "fechafin": ""
-                            }
-
-                        */
+                   
                         setCronograma(ArrcronogramaNulo);
                         
                     }
-                 });
-                // */
-
-
-                 }
+                 });                 }
             },  [])
-
+           
+       
        // console.log('cronograma:',cronograma);
 
         var titulo="Gestión de Bonos";
        
         var botones;
-        
+        console.log("API OBT cro2: ",cronograma);
         if(updateCronograma===true){
-             let apiCronograma = [];
-             const cronGuardado = JSON.parse(localStorage.getItem("Gcronograma")) ;    //La hemos obtenido 
-             apiCronograma.push(cronGuardado)
-             if(apiCronograma){
-                 setCronograma(apiCronograma);
-             }
+            //  let apiCronograma = [];
+            //  const cronGuardado = JSON.parse(localStorage.getItem("Gcronograma")) ;    //La hemos obtenido 
+            //  apiCronograma.push(cronGuardado)
+            //  if(apiCronograma){
+            //      setCronograma(apiCronograma);
+            //  }
              botones=rpta.map((boton) =>   
                         <Grid key={boton.index}  container direction="row" justify="center">
                             <Grid container item md={3} justify="center">                                
@@ -184,8 +222,13 @@ function GestionBonos (props) {
 
             
         }
-
-        if(cronograma[0].idcronograma==="" ){
+        var f = new Date();
+        var escribePantalla=     {
+            nombre:"Bono", //<3
+            // FechaInicio:"2020-12-3", //AAAA-MM-DD
+            FechaInicio:(f.getDate() +1)+ "/" + (f.getMonth() +1) + "/" + f.getFullYear(), //AAAA-MM-DD
+        }
+        if( cronograma[0].idcronograma==="" ){//entra por el api-no hay cronograma
           respuesta=  "Cargando Cronograma...";
             
               //Todoooo la muestra del cronograma está manejado por respuesta
@@ -197,11 +240,35 @@ function GestionBonos (props) {
                      </Typography>
 
                 </Grid>
+                <Grid container direction="col" item md={12} style={{paddingTop: '1.5vh'}}>
+                     <Grid container direction="row" item md={4} style={{paddingTop: '7px'}}>
+                            <Typography variant="subtitle2" color="inherit" >
+                                {'Nombre del Cronograma: '}
+                            </Typography>
+                      </Grid>
+                    {cronograma.map(opcion=> (
+                        <Grid container direction="row" item md={4} >
+                            <Paper component="form"  className={classes.root}>
+   
+                                <InputBase
+                                    className={classes.input}
+                                     placeholder= {"Escriba el nombre de un bono"}                                
+                                    style={{padding:0 }}
+                                    inputProps={{ "aria-label": "Escriba un nombre" }}
+                                    defaultValue={escribePantalla.nombre}
+                                    onChange={e =>guardarNombre(e.target.value)}
+                                />
+                             </Paper>                              
+                        </Grid>
 
+                        )) }
+
+
+                </Grid>
 
                 <Grid container direction="col" item md={12} style={{paddingTop: '1.5vh'}}>
-                     <Grid container direction="row" item md={4} >
-                            <Typography variant="subtitle2" color="inherit">
+                     <Grid container direction="row" item md={4} style={{paddingTop: '7px'}}>
+                            <Typography variant="subtitle2" color="inherit" >
                                 {'Fecha de inicio de reparto: '}
                             </Typography>
                       </Grid>
@@ -211,9 +278,20 @@ function GestionBonos (props) {
                                enviar por params la fecha de inicio, el id mio
                             */}
                             {/* <TextField className="inputRounded" id="outlined-basic" label={null} variant="outlined" /> */}
-                            <Typography variant="subtitle2" color="inherit">
+                            <Paper component="form"  className={classes.root}>
+   
+                                <InputBase
+                                    className={classes.input}
+                                     placeholder= {"Escriba una fecha  DD/MM/AAAA"}                                
+                                    style={{padding:0 }}
+                                    inputProps={{ "aria-label": "Escriba una fecha" }}
+                                    defaultValue={escribePantalla.FechaInicio}
+                                    onChange={e =>guardarFecha(e.target.value)}
+                                />
+                             </Paper>
+                               {/* <Typography variant="subtitle2" color="inherit">
                                     {opcion.fechaini?opcion.fechaini:"Por definir"}
-                            </Typography>
+                            </Typography> */}
                         </Grid>
 
                         )) }
@@ -291,9 +369,12 @@ function GestionBonos (props) {
                             
                     );
             
+        
+         
           
         }
-        else if (cronograma[0].idcronograma!==""){
+        
+        else if (cronograma[0].idcronograma!==""  && cronograma[0].idcronograma!=="inicial" ){
 
             //cambio de formato de Fecha-super no eficiente :(
             var formatoFecha;
@@ -324,11 +405,11 @@ function GestionBonos (props) {
                                         Regresar
                                     </Button>                             
                             </Grid>
-                            {/* <Grid container item md={3} justify="center">                            
-                                <Button variant="contained" size="medium" color="primary" >
+                            <Grid container item md={3} justify="center">                            
+                                <Button variant="contained" size="medium" color="primary" onClick={PublicarCronograma}>
                                     Publicar Cronograma 
                                 </Button>
-                            </Grid> */}
+                            </Grid>
                         </Grid>
                     );
             }
@@ -363,6 +444,25 @@ function GestionBonos (props) {
                 <Grid container direction="col" item md={12} style={{paddingTop: '1.5vh'}}>
                      <Grid container direction="row" item md={4} >
                             <Typography variant="subtitle2" color="inherit">
+                                {'Nombre del Cronograma:  '}
+                            </Typography>
+                      </Grid>
+                    {cronograma.map(opcion=> (
+                        <Grid container direction="row" item md={4} >
+                            <Typography variant="subtitle2" color="inherit">
+                                    {/* Cambiaría y sería por el nombre */}
+                                    {opcion.fechaini?opcion.fechaini:"Por definir"}
+                            </Typography>
+                        </Grid>
+
+                        )) }
+                </Grid>
+
+                
+
+                <Grid container direction="col" item md={12} style={{paddingTop: '1.5vh'}}>
+                     <Grid container direction="row" item md={4} >
+                            <Typography variant="subtitle2" color="inherit">
                                 {'Fecha de inicio de reparto: '}
                             </Typography>
                       </Grid>
@@ -378,9 +478,9 @@ function GestionBonos (props) {
                         </Grid>
 
                         )) }
-
-
                 </Grid>
+
+
                 <Grid container direction="col" item md={12} style={{paddingTop: '1.5vh'}}>
                     <Grid container direction="row" item md={4} >
                         <Typography variant="subtitle2" color="inherit">
@@ -437,7 +537,9 @@ function GestionBonos (props) {
             )
 
         }
-
+        else  if ( cronograma[0].idcronograma==="inicial" ){           
+         respuesta="Cargando..."
+     }
 
 
     return (
