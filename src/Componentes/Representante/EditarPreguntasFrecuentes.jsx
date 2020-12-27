77deef5b7bbd6a9d567519b1f9368a7381cb2a4b
Valeria Urbina <valeria.urbinac@pucp.edu.pe>
2020-12-26 22:25:27 -0500

    Arreglos chiquis

--- src/Componentes/Representante/EditarPreguntasFrecuentes.jsx
+++ src/Componentes/Representante/EditarPreguntasFrecuentes.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { AppBar, Container, Grid, Button } from "@material-ui/core"
+import { Container, Grid, Button } from "@material-ui/core"
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from "react-router-dom"
 import clsx from 'clsx';
